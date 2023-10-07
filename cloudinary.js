ce568488b22646454a605d5283f5bf7d2cd50d0c
Med-Cherif <cherifmohamedi554@gmail.com>
2023-10-07 13:48:34 +0100

    some changes

--- cloudinary.js
+++
@@ -1,9 +0,0 @@
-const cloudinary = require("cloudinary").v2;
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_HOST,
-  api_key: process.env.CLOUDINARY_KEY,
-  api_secret: process.env.CLOUDINARY_SECRERT,
-});
-
-module.exports = cloudinary;
